c03a2516f9100122a15c9d8594c2baaf58e6f59f
ANJANI-GITHUB-MAERSK <104966184+ANJANI-GITHUB-MAERSK@users.noreply.github.com>
2025-01-29 20:47:58 +0530

    Update nameaddressmatch.py

--- nameaddressmatch.py
+++ nameaddressmatch.py
@@ -37,7 +37,7 @@ def jaro_winkler_similarity(str1, str2):
     """Computes the Jaro-Winkler similarity between two strings."""
     st.write('str1 :',str1)
     st.write('str2 :',str2)
-    st.write('jellyfish.jaro_winkler(str1, str2):',jellyfish.jaro_winkler(str1, str2))
+    st.write("jellyfish.jaro_winkler:",jellyfish.jaro_winkler(str1, str2))
     return jellyfish.jaro_winkler(str1, str2)
 
 def match_name_address(df: pd.DataFrame, user_name: str) -> pd.DataFrame:
